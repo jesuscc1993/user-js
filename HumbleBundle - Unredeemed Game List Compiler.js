a289e07b8cb896f714de9d8eb7e6dcc64def3ae7
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2022-09-15 14:42:48 +0200

    Added button.

--- HumbleBundle - Unredeemed Game List Compiler.js
+++ HumbleBundle - Unredeemed Game List Compiler.js
@@ -2,7 +2,7 @@
 // @name         HumbleBundle - Unredeemed Game List Compiler
 // @description  Compiles a list of the unredeemed games
 // @author       MetalTxus
-// @version      1.0.1
+// @version      2022.09.14.23.31
 // @match        https://www.humblebundle.com/home/keys
 // @icon         https://cdn.humblebundle.com/static/hashed/46cf2ed85a0641bfdc052121786440c70da77d75.png
 // ==/UserScript==
@@ -41,5 +41,7 @@
     outputWindow.document.close();
   }
 
-  unsafeWindow.compileGamesList = compileGamesList;
+  const button = jQuery(`<button style="background:#f1f3f6;border:1px solid #ccc;padding:0 12px;">Compile games list</button>`);
+  button.click(compileGamesList);
+  jQuery('.search').before(button);
 })();
