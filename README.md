ee7e5da58a9d5edbeaab2aefae593d3dd1d052d8
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2022-09-15 19:40:26 +0200

    Changed naming pattern and added readme.

---
+++ README.md
@@ -0,0 +1,13 @@
+# UserJS
+
+To install, view the raw files. Requires the
+- [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) Chrome browser extension or the
+- [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) Firefox browser extension
+
+# List
+
+- [global-image-zoomer.user.js](global-image-zoomer.user.js)
+- [humblebundle-unredeemed-game-list-compiler.user.js](humblebundle-unredeemed-game-list-compiler.user.js)
+- [nyaa.si-batch-download.user.js](nyaa.si-batch-download.user.js)
+- [webtoon-keyboard-navigation.user.js](webtoon-keyboard-navigation.user.js)
+- [youtube-remove-watched-videos-button.user.js](youtube-remove-watched-videos-button.user.js)
--- humblebundle-unredeemed-game-list-compiler.user.js
+++ humblebundle-unredeemed-game-list-compiler.user.js
@@ -2,7 +2,7 @@
 // @name         HumbleBundle - Unredeemed Game List Compiler
 // @description  Compiles a list of the unredeemed games
 // @author       MetalTxus
-// @version      2022.09.15.14.43
+// @version      2022.09.15.19.30
 // @match        https://www.humblebundle.com/home/keys
 // @icon         https://cdn.humblebundle.com/static/hashed/46cf2ed85a0641bfdc052121786440c70da77d75.png
 // ==/UserScript==
@@ -37,14 +37,31 @@
 
   const outputGames = () => {
     var outputWindow = window.open('');
-    outputWindow.document.write('<title>HumbleBundle Game List</title>');
+    outputWindow.document.write(`<title>HumbleBundle Game List</title>`);
+    outputWindow.document.write(`<h3>Unredeemed game count: ${games.length}</h3>`);
     outputWindow.document.write(games.sort().join('<br />'));
     outputWindow.document.close();
   }
 
-  const button = jQuery(`<button style="background:#f1f3f6;border:1px solid #ccc;padding:0 12px;">Compile games list</button>`);
-  button.click(compileGamesList);
-  jQuery('.search').before(button);
+  const initialize = () => {
+    jQuery('head').append(`
+      <style>
+        .generate-list-button {
+           background:#f1f3f6;
+           border:1px solid #ccc;
+           padding:0 12px;
+        }
+      </style>
+    `);
 
-  unsafeWindow.compileGamesList = compileGamesList;
+    const button = jQuery(`
+      <button class="generate-list-button">
+        Compile games list
+      </button>
+    `);
+    button.click(compileGamesList);
+    jQuery('.search').before(button);
+  }
+
+  initialize();
 })();
