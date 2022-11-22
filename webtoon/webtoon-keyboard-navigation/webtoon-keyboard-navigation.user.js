a019101fcda6949f72ea56de9474f4e0ba4dd4dd
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2022-11-22 18:40:28 +0100

    Updated webtoon-keyboard-navigation

--- webtoon/webtoon-keyboard-navigation/webtoon-keyboard-navigation.user.js
+++ webtoon/webtoon-keyboard-navigation/webtoon-keyboard-navigation.user.js
@@ -1,38 +1,23 @@
 // ==UserScript==
 // @name           WebToon - Keyboard Navigation
 // @description    Enables pagination with the keyboard arrows
+// @version        2022.11.22.18.40
 // @author         MetalTxus
-// @version        1.0.0
+// @namespace      https://github.com/jesuscc1993
 
 // @icon           https://webtoons-static.pstatic.net/image/favicon/favicon.ico
 // @match          https://www.webtoons.com/*
-// @namespace      https://github.com/jesuscc1993
 // @require        https://code.jquery.com/jquery-3.2.1.min.js
 // @require        https://raw.githubusercontent.com/jesuscc1993/user-js/master/libraries/key-navigation.js
 // ==/UserScript==
 
-/* globals setUpKeyNavigation jQuery */
+/* globals setUpAnchorNavigation jQuery */
 
 (() => {
   'use strict';
 
-  const setUpPagination = () => {
-    const previousPageAnchor = jQuery('.pg_prev');
-    const nextPageAnchor = jQuery('.pg_next');
-
-    setUpKeyNavigation({
-      onLeftPressed: () => {
-        if (previousPageAnchor.length) {
-          location.href = previousPageAnchor.attr('href');
-        }
-      },
-      onRightPressed : () => {
-        if (nextPageAnchor.length && !nextPageAnchor.hasClass('dim')) {
-          location.href = nextPageAnchor.attr('href');
-        }
-      },
-    });
-  }
-
-  setUpPagination();
+  setUpAnchorNavigation({
+    left: '.pg_prev',
+    right: '.pg_next'
+  });
 })();
