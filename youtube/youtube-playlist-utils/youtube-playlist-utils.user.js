d06d9de2ce61769bffdaa05054e91decd5b2eb31
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2024-08-27 22:03:00 +0200

    Migrated away from jQuery.

--- youtube/youtube-playlist-utils/youtube-playlist-utils.user.js
+++ youtube/youtube-playlist-utils/youtube-playlist-utils.user.js
@@ -1,98 +1,123 @@
 // ==UserScript==
 // @name           YouTube - Playlist Utils
 // @description    Adds a length calculation to playlists.
-// @version        2022.11.13.20.26
+// @version        2024.08.27.22.02
 // @author         MetalTxus
 // @namespace      https://github.com/jesuscc1993
 
 // @icon           https://www.youtube.com/favicon.ico
 // @match          https://www.youtube.com/*
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
 
-/* globals jQuery */
-
-const settings = {
-  liteMode: true // add only as global function and not as button; saves on performance
-};
+// const settings = {
+//   liteMode: true // add only as global function and not as button; saves on performance
+// };
 
 (() => {
   'use strict';
 
-  let lengthElement, extraStatsElement, buttonElement;
+  let lengthElement, extraStatsElement /*, buttonElement */;
 
-  const shouldRender = () => {
-    return location.href.includes('/playlist?list=') && jQuery('ytd-thumbnail-overlay-time-status-renderer').length;
-  }
+  // const shouldRender = () => {
+  //   return (
+  //     location.href.includes('/playlist?list=') &&
+  //     document.querySelectorAll('ytd-thumbnail-overlay-time-status-renderer')
+  //       .length
+  //   );
+  // };
 
   const getPlaylistLength = () => {
-    let hours = 0;
-    let minutes = 0;
     let seconds = 0;
 
-    jQuery('ytd-playlist-video-list-renderer ytd-thumbnail-overlay-time-status-renderer').each(
-      (i, el) => {
-        if (el.innerText.includes(':')) {
-          const timeString = el.innerText.replace(/\s*/g, '').split(':');
-          if (timeString.length) seconds += parseInt(timeString.pop(), 10);
-          if (timeString.length) minutes += parseInt(timeString.pop(), 10);
-          if (timeString.length) hours += parseInt(timeString.pop(), 10);
-        }
-      }
+    const badges = document.querySelectorAll(
+      'ytd-playlist-video-list-renderer ytd-thumbnail-overlay-time-status-renderer .badge-shape-wiz__text'
     );
 
-    minutes += Math.floor(seconds / 60);
-    seconds = seconds % 60;
+    badges.forEach((el) => {
+      if (el.innerText.includes(':')) {
+        const timeString = el.innerText.replace(/\s*/g, '').split(':');
+        if (timeString.length) seconds += parseInt(timeString.pop(), 10);
+        if (timeString.length) seconds += parseInt(timeString.pop(), 10) * 60;
+        if (timeString.length) seconds += parseInt(timeString.pop(), 10) * 3600;
+      }
+    });
 
-    hours += Math.floor(minutes / 60);
-    minutes = minutes % 60;
+    return {
+      seconds,
+      videos: badges.length,
+    };
+  };
 
-    return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  }
+  const formatLength = (length) => {
+    const hours = Math.floor(length / 3600);
+    const minutes = Math.floor((length % 3600) / 60);
+    const seconds = length % 60;
+
+    return `${hours ? `${formatTimeToken(hours, false)}:` : ''}${
+      minutes ? `${formatTimeToken(minutes, !!hours)}:` : ''
+    }${formatTimeToken(seconds, !!minutes)}`;
+  };
+
+  const formatTimeToken = (token, shouldPad) => {
+    return shouldPad ? String(token).padStart(2, '0') : token;
+  };
 
   const calculateExtraPlaylistStats = () => {
-    const containerElement = jQuery('ytd-playlist-byline-renderer').first();
-    if (containerElement.length && !containerElement.find(extraStatsElement).length) {
-      containerElement.find('.metadata-stats').prepend(extraStatsElement);
+    const containerElement = document.querySelector(
+      'ytd-playlist-byline-renderer'
+    );
+    if (containerElement && !containerElement.querySelector('.extra-stats')) {
+      containerElement
+        .querySelector('.metadata-stats')
+        .prepend(extraStatsElement);
     }
 
     const playlistLength = getPlaylistLength();
-    console.log(`Playlist length: ${playlistLength}`);
-    lengthElement.html(`Length: ${playlistLength}&nbsp;`);
-  }
+    console.log(`Extra playlist stats:
+  Videos:
+    ${playlistLength.videos}
+  Length:
+    ${formatLength(playlistLength.seconds)}
+  Length on average:
+    ${formatLength(
+      Math.round(playlistLength.seconds / playlistLength.videos)
+    )}`);
+    /* lengthElement.innerHTML = `Length: ${playlistLength}&nbsp;`; */
+  };
+
+  // const handleButtonPresence = () => {
+  //   if (shouldRender()) {
+  //     const buttonContainerElement = jQuery('.ytd-playlist-byline-renderer').first();
+  //     if (buttonContainerElement.length && !buttonContainerElement.find('#stats').before(buttonElement).length) {
+  //       buttonContainerElement.prepend(buttonElement);
+  //     }
+  //   } else {
+  //     buttonElement.remove();
+  //   }
+  // };
 
   const initialize = () => {
-    lengthElement = jQuery(`<span class="ytd-playlist-sidebar-primary-info-renderer"></span>`);
+    lengthElement = document.createElement('span');
+    lengthElement.className = 'ytd-playlist-sidebar-primary-info-renderer';
 
-    extraStatsElement = jQuery(`<span id="extra-stats" class="style-scope ytd-playlist-sidebar-primary-info-renderer"></span>`);
-    extraStatsElement.append(lengthElement);
+    extraStatsElement = document.createElement('span');
+    extraStatsElement.className =
+      'extra-stats style-scope ytd-playlist-sidebar-primary-info-renderer';
+    extraStatsElement.appendChild(lengthElement);
 
     unsafeWindow.calculateExtraPlaylistStats = calculateExtraPlaylistStats;
 
-    /*buttonElement = jQuery(`
-      <tp-yt-paper-button class="style-scope ytd-subscribe-button-renderer" style="margin-top: 24px;">
-        Calculate Extra Stats
-      </tp-yt-paper-button>
-    `);
-    buttonElement.click(calculateExtraPlaylistStats);
+    // buttonElement = jQuery(`
+    //   <tp-yt-paper-button class="style-scope ytd-subscribe-button-renderer" style="margin-top: 24px;">
+    //     Calculate Extra Stats
+    //   </tp-yt-paper-button>
+    // `);
+    // buttonElement.click(calculateExtraPlaylistStats);
 
-    const handleButtonPresence = () => {
-      if (shouldRender()) {
-        const buttonContainerElement = jQuery('.ytd-playlist-byline-renderer').first();
-        if (buttonContainerElement.length && !buttonContainerElement.find('#stats').before(buttonElement).length) {
-          buttonContainerElement.prepend(buttonElement);
-        }
-      } else {
-        buttonElement.remove();
-      }
-    };
-
-    const observer = new MutationObserver(handleButtonPresence);
-    observer.observe(document.body, { childList: true, subtree: true });
-
-    handleButtonPresence();*/
-  }
+    // const observer = new MutationObserver(handleButtonPresence);
+    // observer.observe(document.body, { childList: true, subtree: true });
+    // handleButtonPresence();
+  };
 
   initialize();
-
 })();
