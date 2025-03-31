183be7c78575ed8e3bc6ac1c900d72c3927be896
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-03-31 21:14:48 +0200

    Updated icon.

--- discord/discord-utils/discord-utils.user.js
+++ discord/discord-utils/discord-utils.user.js
@@ -1,25 +1,25 @@
 // ==UserScript==
 // @name           Discord - Utils
 // @description    Replaces Discord's favicon with the classic one and adds controls to the embedded videos.
-// @version        2022.11.22.18.26
+// @version        2025.03.31.21.13
 // @author         MetalTxus
 // @namespace      https://github.com/jesuscc1993
 
-// @icon           https://i.imgur.com/UP6UJmx.png
+// @icon           https://i.imgur.com/l9A3eFO.png
 // @match          https://discord.com/*
 // @require        http://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
 
 /* globals jQuery */
 
-(function() {
+(function () {
   'use strict';
 
   const addVideoControls = () => {
     jQuery('video[class*="embedVideo"]').attr('controls', '');
-  }
+  };
 
   setInterval(addVideoControls, 1000);
 
-  jQuery('link[rel="icon"]').attr('href', 'https://i.imgur.com/UP6UJmx.png');
+  jQuery('link[rel="icon"]').attr('href', 'https://i.imgur.com/l9A3eFO.png');
 })();
