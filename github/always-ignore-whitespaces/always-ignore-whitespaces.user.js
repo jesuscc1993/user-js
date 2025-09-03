fd8358ff9d89ca69824cf67cdce8124f3f72d288
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2025-09-03 11:36:04 +0200

    Extended script to apply to comparse too.

--- github/always-ignore-whitespaces/always-ignore-whitespaces.user.js
+++ github/always-ignore-whitespaces/always-ignore-whitespaces.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name           (GitHub) Always ignore whitespace
 // @description    Always ignore whitespace in GitHub PR diffs
-// @version        2025.08.25.15.15
+// @version        2025.09.03.11.35
 // @author         MetalTxus
 // @namespace      https://github.com/jesuscc1993
 
 // @match          https://github.com/*/pull/*/files*
+// @match          https://github.com/*/compare*
 // @run-at         document-start
 
 // @icon           https://github.githubassets.com/favicons/favicon-dark.png
