f650bc8bd07ce31ad94c28e29e71feb4336e73ba
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-09-09 17:27:31 +0200

    Added SORT_BY_DISCOUNT setting.

--- allkeyshop/discount-calculator/allkeyshop-discount-calculator.user.js
+++ allkeyshop/discount-calculator/allkeyshop-discount-calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           AllKeyShop - Discount Calculator
 // @description    Calculates discounts against the best official price.
-// @version        2025.09.09.17.16
+// @version        2025.09.09.17.27
 // @author         MetalTxus
 
 // @match          https://www.allkeyshop.com/blog/list/*
@@ -9,29 +9,36 @@
 
 /* globals jQuery */
 
+const SORT_BY_DISCOUNT = true;
+
 (() => {
   'use strict';
 
+  let rowsWithDiscount = [];
+
   jQuery('.game-row').each((_, e) => {
-    const officialPriceEl = jQuery(e).find('.game-best-official-price');
-    const bestPriceEl = jQuery(e).find('.game-best-price');
+    const $officialPrice = jQuery(e).find('.game-best-official-price');
+    const $bestPrice = jQuery(e).find('.game-best-price');
 
-    const officialPrice = parseFloat(officialPriceEl.text());
-    const bestPrice = parseFloat(bestPriceEl.text());
-
-    if (
-      isNaN(officialPrice) ||
-      isNaN(bestPrice) ||
-      bestPrice == officialPrice
-    ) {
-      return;
-    }
+    const officialPrice = parseFloat($officialPrice.text());
+    const bestPrice = parseFloat($bestPrice.text());
 
     const discount = Math.round(
       ((officialPrice - bestPrice) / officialPrice) * 100
     );
-    bestPriceEl.append(
-      `<span class="metacritic-button-green">&nbsp;-${discount}%&nbsp;</span>`
-    );
+    if (discount > 0) {
+      $bestPrice.append(
+        `<span class="metacritic-button-green">&nbsp;-${discount}%&nbsp;</span>`
+      );
+    }
+
+    rowsWithDiscount.push({ discount, $row: jQuery(e) });
   });
+
+  if (SORT_BY_DISCOUNT) {
+    rowsWithDiscount.sort((a, b) => (a.discount < b.discount ? 1 : -1));
+
+    const $container = jQuery('.akswl-list > tbody');
+    jQuery.each(rowsWithDiscount, (_, { $row }) => $container.append($row));
+  }
 })();
