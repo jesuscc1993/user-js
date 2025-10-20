ee6bab9456df249064b4e6d6fad99d16e5112aa3
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2025-10-20 10:49:35 +0200

    Added small fallback.

--- github/always-ignore-whitespaces/always-ignore-whitespaces.user.js
+++ github/always-ignore-whitespaces/always-ignore-whitespaces.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name           (GitHub) Always ignore whitespace
 // @description    Always ignore whitespace in GitHub PR diffs
-// @version        2025.09.03.11.35
+// @version        2025.10.20.14.49
 // @author         MetalTxus
 // @namespace      https://github.com/jesuscc1993
 
+// @match          https://github.com/*/pull/*
 // @match          https://github.com/*/pull/*/files*
 // @match          https://github.com/*/compare*
 // @run-at         document-start
@@ -15,9 +16,17 @@
 (() => {
   'use strict';
 
-  const url = new URL(window.location.href);
-  if (!url.searchParams.has('w')) {
-    url.searchParams.set('w', '1');
-    window.location.replace(url.toString());
-  }
+  const prPathnameRegex = /^\/.+\/pull\/\d+\/files/;
+
+  const replaceUrl = () => {
+    const url = new URL(window.location.href);
+    if (prPathnameRegex.test(url.pathname) && !url.searchParams.has('w')) {
+      url.searchParams.set('w', '1');
+      window.location.replace(url.toString());
+    }
+  };
+  replaceUrl();
+
+  /* the idea for this fallback is to give enough time to navigate to the correct tab after opening a PR */
+  setTimeout(replaceUrl, 5000);
 })();
